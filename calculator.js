b168d4645815e742ecb078cd158ad331f7a45022
Abhidofttech <abhisofttech360@gmail.com>
2024-02-26 21:36:28 +0530

    made responsive for other devices

--- calculator.js
+++ calculator.js
@@ -1,19 +1,27 @@
 function num(a) {
    document.getElementById('screen').value += a;
-
 }
+
 function op(a) {
-   document.getElementById('screen').value += a;
+   let screenValue = document.getElementById('screen').value;
+   let lastChar = screenValue[screenValue.length - 1];
 
+   // Check if the last character is an operator, if yes, replace it with the new operator
+   if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
+       document.getElementById('screen').value = screenValue.slice(0, -1) + a;
+   } else {
+       document.getElementById('screen').value += a;
+   }
 }
+
 function clear1() {
-   document.getElementById('screen').value = ' ';
-
+   document.getElementById('screen').value = '';
 }
+
 function total() {
    if (document.getElementById('screen').value == 0) {
-      document.getElementById('screen').value = '';
+       document.getElementById('screen').value = '';
    } else {
-      document.getElementById('screen').value = eval(document.getElementById('screen').value);
+       document.getElementById('screen').value = eval(document.getElementById('screen').value);
    }
 }
